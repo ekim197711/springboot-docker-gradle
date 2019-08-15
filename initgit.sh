cd7b334bd39aff0b9c429f3415f4616f09d3ae21
Mike Møller Nielsen <mmni@protonmail.com>
2019-08-15 19:38:44 +0200

    the code

---
+++ initgit.sh
@@ -0,0 +1,6 @@
+echo "# springboot-docker-gradle" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:ekim197711/springboot-docker-gradle.git
+git push -u origin master
